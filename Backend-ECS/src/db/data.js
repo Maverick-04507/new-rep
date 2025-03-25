dea997bf8b48054d61df19db0016840f2a9ed042
Maverick-04507 <Shashwatp673@gmail.com>
2025-03-25 22:12:49 +0530

    questions add

--- Backend-ECS/src/db/data.js
+++ Backend-ECS/src/db/data.js
@@ -6,5 +6,21 @@ export const questions = [
   {
       question: "What is Node.js?",
       answer: "backend runtime",
-  }
+  },
+  {
+    question: "What is full Name of Iron Man?",
+    answer: "Antony Edward Stark",
+},
+{
+  question: "Who plays captain america?",
+  answer: "Chris Evans",
+}
+,
+{
+  question: "What is more sexy Scarlett Johansson or Elizabeth Olsen?",
+  answer: "Scarlett Johansson",
+}, {
+  question: "Who plays Black Widow?",
+  answer: "Scarlett Johansson",
+}
 ];
